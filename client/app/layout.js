5b726c21ecc03c083db872527d0d9a46406994c6
momenazhar <momenazher57@gmail.com>
2024-02-23 02:25:26 +0300

    styles

--- client/app/layout.js
+++ client/app/layout.js
@@ -14,8 +14,26 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <div className="absolute">
-                    <Image src="/background.svg" height={800} width={800} />
+                <div>
+                    <Image
+                        src="/background.svg"
+                        height={800}
+                        width={800}
+                        alt="Background Gradient"
+                        className="absolute"
+                    />
+                    <Image
+                        src="/background2.png"
+                        height={800}
+                        width={800}
+                        alt="Background Gradient"
+                        className="absolute bottom-0 right-0 rotate-180 h-3/4 w-3/4"
+                    />
+                </div>
+                <div className="absolute w-svw bottom-0 overflow-hidden bg-indigo-100/60 border-1 border-indigo-200/40 select-none flex flex-row items-center justify-around font-bold text-indigo-400/55 tracking-wider py-3">
+                    <p className="drop-shadow-md">Fatima Jamal</p>
+                    <p className="drop-shadow-md">Momen Azhar</p>
+                    <p className="drop-shadow-md">Zhir Salahaddin</p>
                 </div>
                 <Providers>{children}</Providers>
             </body>
--- client/app/page.js
+++ client/app/page.js
@@ -1,38 +1,55 @@
-import { Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
+import { Card, CardBody, CardFooter } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="h-svh w-svw flex flex-row justify-center items-center gap-10 p-4">
-            <Card className="max-w-[250px] h-[300px] bg-white/50">
-                <CardBody className="flex flex-col items-center justify-center  mt-5">
-                    <p className="text-3xl font-bold text-slate-500  drop-shadow-md">Sender</p>
-                </CardBody>
-                <CardFooter className="bg-slate-200/80">
-                    <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm">
-                        Encrypt and send a message with a symmetric key
-                    </p>
-                </CardFooter>
-            </Card>
-            <Card className="max-w-[250px] h-[300px] bg-white/50">
-                <CardBody className="flex flex-col items-center justify-center mt-5">
-                    <p className="text-3xl font-bold text-slate-500  drop-shadow-md">Receiver</p>
-                </CardBody>
-                <CardFooter className="bg-slate-200/80">
-                    <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm">
-                        Receive and decrypt with a symmetric key
-                    </p>
-                </CardFooter>
-            </Card>
-            <Card className="max-w-[250px] h-[300px] bg-white/50">
-                <CardBody className="flex flex-col items-center justify-center  mt-5">
-                    <p className="text-3xl font-bold text-slate-500  drop-shadow-md">Attacker</p>
-                </CardBody>
-                <CardFooter className="bg-slate-200/80">
-                    <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm">
-                        Receive a cipher text and break it
-                    </p>
-                </CardFooter>
-            </Card>
+        <div className="h-svh flex flex-col gap-8 justify-center items-center">
+            <div className="w-svw text-center flex gap-5 flex-col">
+                <h1 className="text-5xl font-extrabold text-slate-500 select-none drop-shadow-md">
+                    Caesar Cipher Encryption
+                </h1>
+                <p className="text-2xl text-slate-400 select-none drop-shadow-sm">
+                    Presentation of Caesar Cipher encryption method using symmetric key between users
+                </p>
+            </div>
+            <div className="h-1/2 w-svw flex flex-row justify-center items-center gap-24 p-4">
+                <Link href="/sender">
+                    <Card className="max-w-[250px] h-[300px] bg-white/50 cursor-pointer hover:translate-y-[-16px] hover:shadow-large hover:transition-all hover:bg-white/95">
+                        <CardBody className="flex flex-col items-center justify-center mt-5">
+                            <p className="text-3xl font-bold text-slate-500  drop-shadow-md select-none">Sender</p>
+                        </CardBody>
+                        <CardFooter className="bg-slate-200/70">
+                            <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm select-none">
+                                Encrypt and send a message with a symmetric key
+                            </p>
+                        </CardFooter>
+                    </Card>
+                </Link>
+                <Link href="/receiver">
+                    <Card className="max-w-[250px] h-[300px] bg-white/50 cursor-pointer hover:translate-y-[-16px] hover:shadow-large hover:transition-all hover:bg-white/95">
+                        <CardBody className="flex flex-col items-center justify-center mt-5">
+                            <p className="text-3xl font-bold text-slate-500  drop-shadow-md select-none">Receiver</p>
+                        </CardBody>
+                        <CardFooter className="bg-slate-200/70">
+                            <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm select-none">
+                                Receive and decrypt with a symmetric key
+                            </p>
+                        </CardFooter>
+                    </Card>
+                </Link>
+                <Link href="attacker">
+                    <Card className="max-w-[250px] h-[300px] bg-white/50 cursor-pointer hover:translate-y-[-16px] hover:shadow-large hover:transition-all hover:bg-white/95">
+                        <CardBody className="flex flex-col items-center justify-center mt-5">
+                            <p className="text-3xl font-bold text-slate-500  drop-shadow-md select-none">Attacker</p>
+                        </CardBody>
+                        <CardFooter className="bg-slate-200/70">
+                            <p className="text-md font-regular text-slate-400 text-center drop-shadow-sm select-none">
+                                Receive a cipher text and break it
+                            </p>
+                        </CardFooter>
+                    </Card>
+                </Link>
+            </div>
         </div>
     );
 }
